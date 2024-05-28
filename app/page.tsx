3c596bca2e04967e120a5613126edd26fa0a2055
Ali Alipoor <alialipoordev@gmail.com>
2024-05-28 18:38:08 +0330

    navbar-hero sections styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Hero } from "@/components";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden"></main>
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
   );
 }
